86cc3ef2c25c9009537967d2b3bb7b68a66aa7d7
Jegathish-16 <jegathish162004@gmail.com>
2023-03-31 16:32:19 +0530

    Add.Code in the subscription.jsx and Update.Code in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Subscription from './Component/Subscription';
+
 function App() {
   return (
     <div className="App">
-      <Subscription/>
+        <Subscription/>
     </div>
   );
 }
